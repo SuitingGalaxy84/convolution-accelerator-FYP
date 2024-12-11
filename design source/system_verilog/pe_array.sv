61cc4340c6f66490c44eb4feac22d4d8f454df97
Yucheng Sun <lambertsun84@outlook.com>
2024-12-11 14:50:30 +0800

    build pe array skeleton

--- design source/system_verilog/pe_array.sv
+++ design source/system_verilog/pe_array.sv
@@ -31,8 +31,10 @@ module xbus(
         BUS_CTRL #(DATA_WIDTH, NUM_COL, NUM_ROW) UniV_BUS_CTRL
     );
 
-    BUS_IF #(DATA_WIDTH) UniV_XBUS_IF;
-    
+    BUS_IF #(DATA_WIDTH) UniV_XBUS_IF [NUM_ROW-1:0];
+    PE_ITR #(DATA_WIDTH) PE_IITR [NUM_ROW-1:0][NUM_COL-1:0];
+    PE_ITR #(DATA_WIDTH) PE_OITR [NUM_ROW-1:0][NUM_COL-1:0];
+
     
 
     X_BusCtrl #(
@@ -47,22 +49,35 @@ module xbus(
         .UniV_XBUS_IF(UniV_XBUS_IF),
         .UniV_BUS_CTRL(UniV_BUS_CTRL)
     )
-
-    genvar i;
+    
+    genvar i, j;
     generate
-        for(i=0; i<NUM_ROW; i=i+1) begin: GLB_PE_INST
-            
-            glb_PE #(
-                .DATA_WIDTH(DATA_WIDTH),
-                .NUM_COL(NUM_COL)
-            ) glb_PE_inst(
-                .clk(clk),
-                .rstn(rstn),
-                .external(external),
-                .BUS_IF(UniV_XBUS_IF),
+        for(i=0; i<NUM_ROW; i=i+1) begin : row_interface
 
-            )
+            for(j=0; j<NUM_COL; j=j+1) begin : col_interface
+            end 
         end 
+
+    endgenerate
+    genvar i, j;
+    generate // instantiate Global PE Array
+        for(i=0; i<NUM_ROW; i=i+1) begin : row_gen
+            for(j=0; j<NUM_COL; j=j+1) begin : col_gen
+                glb_PE #(
+                    .DATA_WIDTH(DATA_WIDTH),
+                    .NUM_COL(NUM_COL)
+                ) glb_PE_inst(
+                    .clk(clk),
+                    .rstn(rstn),
+                    .external(external),
+                    .BUS_IF(UniV_XBUS_IF[i]),
+                    .PE_ITR(PE_IITR[i][j]),
+                    .PE_ITR(PE_OITR[i][j])
+                )
+
+            end 
+        end 
+
     endgenerate
 
 endmodule
