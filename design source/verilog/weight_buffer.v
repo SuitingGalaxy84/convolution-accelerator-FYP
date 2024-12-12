4231dc6e4522dfb02e592fed10979e2fd6dd8937
Yucheng Sun <lambertsun84@outlook.com>
2024-12-12 11:31:41 +0800

    weight_buffer dut

--- design source/verilog/weight_buffer.v
+++ design source/verilog/weight_buffer.v
@@ -34,23 +34,60 @@ module WeightBuff #(
     output [DATA_WIDTH-1:0] pseudo_out,
     output flush_READY,
 
-    input PE_VALID,
-    output FLTR_READY
+    input READY
+    output VALID
     
 );
 
     reg [DATA_WIDTH-1:0] weight_buff [BUFFER_DEPTH-1:0];
     reg [7:0] wr_ptr;
+    reg [7:0] rd_ptr;
+    reg [7:0] next_rd_ptr;
+
     assign flush_READY = (wr_ptr==kernel_size-1) ? 1 : 0;
+    assign read_VALID = (rd_ptr==kernel_size-1) ? 1 : 0;
+
 
     localparam FLUSH_IDLE = 2'b00;
     localparam FLUSH_START = 2'b10;
     localparam FLUSH_READY = 2'b01;
     localparam FLUSH_RSET = 2'b11;
 
-    
+    localparam READ_IDEL = 2'b0;
+    localparam READ_OP = 2'b1;
+
     wire [1:0] flush_ctrl = {flush, flush_READY};
-    assign data_out = weight_buff[kernel_size-1];
+    
+    reg read_currnet_state;
+    reg read_next_state;
+
+    assign pseudo_out = weight_buff[BUFFER_DEPTH-1:0];
+    assign data_out = weight_buff[rd_ptr];
+
+    always @(*) begin
+        case(read_currnet_state)
+            READ_IDEL: begin
+                read_next_state = READY ? READ_OP : READ_IDEL;
+                next_rd_ptr = 0;
+            end 
+            READ_OP: begin
+                next_rd_ptr = rd_ptr+1;
+                read_next_state = (rd_ptr==kernel_size-1) ? READ_IDEL : READ_OP;
+            end
+        endcase
+    end  
+
+    always @(posedge clk or rstn) begin // read
+        if(~rstn) begin
+            rd_ptr <= 0;
+            read_currnet_state = 0;
+        end else begin
+            rd_ptr <= next_rd_ptr;
+            read_currnet_state <= read_next_state;
+        end 
+    end 
+
+
     integer i;
     always @(posedge clk or negedge rstn) begin // write
         if(~rstn) begin
